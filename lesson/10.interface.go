10a00fbcc3f69ed880051e7d990a066b2f1ac7c9
bruce <baibaibai_000@163.com>
2018-04-07 21:39:41 +0800

    interface test

---
+++ lesson/10.interface.go
@@ -0,0 +1,49 @@
+package lesson
+
+import(
+	"fmt"
+	"math"
+)
+
+type geometry interface{
+	area() float64
+	perim() float64
+}
+
+type rect struct{
+	width,height float64
+}
+
+type circle struct{
+	radius float64
+}
+
+func (re rect) area() float64{
+	return re.width*re.height
+}
+
+func (re rect)perim() float64{
+	return 2*re.width+2*re.height
+}
+
+func (c circle)area() float64{
+	return math.Pi*c.radius*c.radius
+}
+
+func (c circle)perim() float64{
+	return 2*math.Pi*c.radius
+}
+
+func measure(g geometry){
+	fmt.Println(g)
+	fmt.Println(g.area())
+	fmt.Println(g.perim())
+}
+
+func InterfaceTest(){
+	r := rect{width:10,height:20}
+	c := circle{radius:10}
+
+	measure(r)
+	measure(c)
+}
