651ff040b3192ead38d373be27327a3a4b9ca130
bruce <baibaibai_000@163.com>
2018-04-08 18:07:15 +0800

    test

--- main.go
+++ main.go
@@ -1,19 +1,51 @@
 package main
 
-import (
-	"fmt"
-	"golesson/lesson"
-)
+//"golesson/lesson"
+//"golesson/advance"
+//"golesson/thread"
 
 func main() {
-	fmt.Println("hello world")
+	//fmt.Println("hello world")
 
 	//lesson.VariantTest()
-	
+
 	//lesson.ForTest()
 
 	//lesson.IfTest()
 
-	lesson.SliceTest()
+	//lesson.SliceTest()
 
-	}
+	//lesson.SwitchTest()
+
+	//lesson.ErrorTest()
+
+	//lesson.ChanTest()
+
+	//lesson.SelectTest()
+
+	//lesson.Fab()
+
+	//lesson.TimeoutTest(1, 2)
+
+	//lesson.RangeChanTest()
+
+	//lesson.TimeTest()
+
+	//lesson.WorkTest()
+
+	//lesson.LimiterTest()
+
+	//lesson.AutomicTest()
+
+	//lesson.MutexTest()
+
+	//lesson.SortTest()
+
+	//lesson.DeferTest()
+
+	//lesson.StringTest()
+
+	//lesson.JsonTest()
+
+	//lesson.TimeTest()
+}
