4d2c3d6f97d8b6e407ed5ecbe2b186c88b8c4df5
bruce <baibaibai_000@163.com>
2018-04-09 11:04:06 +0800

    string test

--- misc/4.string.go
+++ misc/4.string.go
@@ -10,16 +10,15 @@ var p = fmt.Println
 
 func StringTest() {
 
-	p("Contains:  ", s.Contains("test", "es"))
-	p("Count:     ", s.Count("test", "t"))
-	p("HasPrefix: ", s.HasPrefix("test", "te"))
-	p("HasSuffix: ", s.HasSuffix("test", "st"))
-	p("Index:     ", s.Index("test", "e"))
-	p("Join:      ", s.Join([]string{"a", "b"}, "-"))
-	p("Repeat:    ", s.Repeat("a", 5))
-	p("Replace:   ", s.Replace("foo", "o", "0", -1))
-	p("Replace:   ", s.Replace("foo", "o", "0", 1))
-	p("Split:     ", s.Split("a-b-c-d-e", "-"))
-	p("ToLower:   ", s.ToLower("TEST"))
-	p("ToUpper:   ", s.ToUpper("test"))
+	p("Contains:	", s.Contains("test", "es"))
+	p("Count:		", s.Count("test", "t"))
+	p("HasPrefix:	", s.HasPrefix("test", "te"))
+	p("HasSuffix:	", s.HasSuffix("test", "st"))
+	p("Join:		", s.Join([]string{"a", "b", "c"}, "-"))
+	p("Repeat:		", s.Repeat("ab", 5))
+	p("Replace:		", s.Replace("test", "t", "1", -1))
+	p("Replace:		", s.Replace("test", "t", "1", 0))
+	p("Split:		", s.Split("1,2,3,4", ","))
+	p("ToLower:		", s.ToLower("ABCD"))
+	p("ToUpper:		", s.ToUpper("abcD"))
 }
