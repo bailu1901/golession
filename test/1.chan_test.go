117b41b009f96f7ed0f54484f8932c23d3b05527
bruce <baibaibai_000@163.com>
2018-05-07 18:37:40 +0800

    增加测试

--- test/1.chan_test.go
+++ test/1.chan_test.go
@@ -2,7 +2,9 @@ package test
 
 import (
 	"fmt"
+	"sync"
 	"testing"
+	"time"
 )
 
 type mytype struct {
@@ -11,29 +13,99 @@ type mytype struct {
 }
 
 func TestChanValue(t *testing.T) {
-	{
-		c := make(chan *mytype, 1)
 
-		a := &mytype{a: 1, b: 2}
-		c <- a
-		temp := <-c
-		temp.a = 10
-		fmt.Println(a, temp)
-		t.Log(*a, *temp)
+	/*
+		{
+			c := make(chan *mytype, 1)
+
+			a := &mytype{a: 1, b: 2}
+			c <- a
+			temp := <-c
+			temp.a = 10
+			fmt.Println(a, temp)
+			t.Log(*a, *temp)
+		}
+
+		{
+			a := [5]int{1, 2}
+
+			c := make(chan [5]int, 1)
+			c <- a
+			a1 := <-c
+
+			a1[0] = 2
+
+			fmt.Println(a, a1)
+		}
+
+		{
+			c := make(chan int)
+			go func() {
+				i := 0
+				ticker := time.NewTicker(time.Second)
+				for {
+					select {
+					case <-ticker.C:
+						c <- i
+					}
+					i++
+					if 10 == i {
+						close(c)
+					}
+				}
+
+			}()
+
+			for r := range c {
+				fmt.Println(r)
+			}
+		}
+	*/
+
+	{
+		closeChan := make(chan bool)
+
+		wg := sync.WaitGroup{}
+		for i := 0; i < 10; i++ {
+			wg.Add(1)
+			go func(idx int) {
+
+				for {
+					select {
+					case v := <-closeChan:
+						fmt.Println("over", idx, v)
+						wg.Done()
+						return
+					}
+				}
+			}(i)
+
+		}
+
+		closeChan <- true
+		time.Sleep(time.Second)
+		close(closeChan)
+
+		wg.Wait()
 	}
 
-	{
-		a1 := [5]int{1, 2}
+	{ /*
+			closeChan := make(chan int, 10)
 
-		c := make(chan []int, 1)
-		a := make([]int, 10)
-		a[0] = 1
-		a[1] = 2
-		c <- a
+			go func() {
 
-		temp := <-c
-		a[0] = 10
-		t.Log(a1, a, temp)
+				for i := 0; i < 10; i++ {
+					closeChan <- i
+
+					time.Sleep(time.Millisecond * 200)
+				}
+				close(closeChan)
+			}()
+
+			for v := range closeChan {
+				fmt.Println(v)
+			}
+		*/
 	}
 
 }
